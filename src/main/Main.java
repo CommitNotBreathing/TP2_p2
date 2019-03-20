1939314e7ab186bbb7318f7a4acfcd8fbf6627cd
Pascal Raymond <pascalraymd@gmail.com>
2019-03-20 09:42:00 -0400

    Debut du projet

---
+++ src/main/Main.java
@@ -0,0 +1,43 @@
+package main;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+
+
+
+public class Main {
+	
+	public static void principale() {
+	
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\Style.txt"));
+			ArrayList<String> info = new ArrayList<String>();
+			String line;
+			while ((line = br.readLine()) !=null) {
+					info.add(line);
+			}
+			
+			br.close();
+			
+			Client.CreateClients(info);
+			Plat.CreatePlats(info);
+			Commande.CreateCommande(info);
+			Facture.PrintFacture();
+			
+		} catch (Exception e) {
+			System.out.println("Le fichier ne respecte pas le format demandé !");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		principale();
+	}
+
+
+}
+
+
